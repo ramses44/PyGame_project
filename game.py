1dc1f76e6eb3acfcaef63e8b98d2a73f8ea2a142
ramses44 <romis2002@mail.ru>
2020-01-17 19:41:15 +0300

    Добавлены картинки спрайтов лестниц, платформ и персонажа. Персонаж нормально перемещается по платформам и лестницам (при наложении платформ может проваливаться)

--- game.py
+++ game.py
@@ -1,7 +1,28 @@
 import pygame
+import os
 
-CHARACTER_SIZE = 30, 30
+CHARACTER_SIZE = 60, 100
+PLATFORM_SIZE = 150, 25
+LADDER_SIZE = 50, 120
 FALLING_SPEED = 2  # Скорость падения (pixels/tick)
+LEFT, RIGHT = False, True
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    image = pygame.image.load(fullname)
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+        image.set_alpha(0)
+
+    return image
+
 
 pygame.init()
 
@@ -16,53 +37,82 @@ ladders = pygame.sprite.Group()
 
 
 class Platform(pygame.sprite.Sprite):
+    image = load_image("platform.png")
+
     def __init__(self, group, pos):
         super().__init__(group)
-        self.image = pygame.Surface((50, 10))
-        pygame.draw.rect(self.image, (128, 128, 128), (0, 0, 50, 10))
-        self.rect = pygame.Rect(*pos, 50, 10)
+        self.image = pygame.transform.scale(Platform.image, PLATFORM_SIZE)
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x, self.rect.y = pos
 
 
 class Ladder(pygame.sprite.Sprite):
+    image = load_image("ladder.png")
+
     def __init__(self, group, pos):
         super().__init__(group)
-        self.image = pygame.Surface((10, 50))
-        pygame.draw.rect(self.image, (254, 0, 0), (0, 0, 10, 50))
-        self.rect = pygame.Rect(*pos, 10, 50)
+        self.image = pygame.transform.scale(Ladder.image, LADDER_SIZE)
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x, self.rect.y = pos
 
 
 class Enemy(pygame.sprite.Sprite):
+    image1 = load_image("character_1.png")
+    image2 = load_image("character_2.png")
+
     def __init__(self, group, pos):
         super().__init__(group)
-        self.image = pygame.Surface(CHARACTER_SIZE)
-        pygame.draw.rect(self.image, (0, 0, 255), (0, 0, *CHARACTER_SIZE))
-        self.rect = pygame.Rect(*pos, *CHARACTER_SIZE)
+        self.active_step = 0
+        self.rotation = RIGHT
+        self.image = pygame.transform.scale(Enemy.image1, CHARACTER_SIZE)
+        self.rect = self.image.get_rect()
         self.climbing = False
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x, self.rect.y = pos
 
     def move(self, x=0, y=0):
 
-        delta = 1 if x > 0 else -1
+        if x > 0:
+            delta = 1
+            self.step(RIGHT)
+        elif x < 0:
+            delta = -1
+            self.step(LEFT)
+
         for _ in range(*sorted((x, 0))):
             self.rect = self.rect.move(delta, 0)
-            if pygame.sprite.spritecollideany(self, platforms):
+
+            s = pygame.sprite.spritecollideany(self, platforms)
+            if s and pygame.sprite.collide_mask(self, s):
                 self.rect = self.rect.move(-delta, 0)
                 break
-            if pygame.sprite.spritecollideany(self, barrels):
+
+            s = pygame.sprite.spritecollideany(self, barrels)
+            if s and pygame.sprite.collide_mask(self, s):
                 return True
 
         delta = 1 if y > 0 else -1
         for _ in range(*sorted((y, 0))):
             self.rect = self.rect.move(0, delta)
-            if pygame.sprite.spritecollideany(self, platforms) and not self.climbing:
+
+            s = pygame.sprite.spritecollideany(self, platforms)
+            if s and pygame.sprite.collide_mask(self, s):
                 self.rect = self.rect.move(0, -delta)
                 break
-            if pygame.sprite.spritecollideany(self, barrels):
+
+            s = pygame.sprite.spritecollideany(self, barrels)
+            if s and pygame.sprite.collide_mask(self, s):
                 return True
-            if pygame.sprite.spritecollideany(self, ladders) and not self.climbing:
+
+            s = pygame.sprite.spritecollideany(self, ladders)
+            if s and pygame.sprite.collide_mask(self, s) and not self.climbing:
                 self.climbing = True
                 break
 
-        if pygame.sprite.spritecollideany(self, ladders):
+        s = pygame.sprite.spritecollideany(self, ladders)
+        if s and pygame.sprite.collide_mask(self, s):
             self.climbing = True
         else:
             self.climbing = False
@@ -72,10 +122,37 @@ class Enemy(pygame.sprite.Sprite):
 
         self.rect = self.rect.move(0, 1)
 
-        res = bool(pygame.sprite.spritecollideany(self, platforms) or
-                   pygame.sprite.spritecollideany(self, ladders))
+        res = pygame.sprite.spritecollideany(self, platforms)
+        if not res:
+            res = pygame.sprite.spritecollideany(self, ladders)
+        res = bool(res and pygame.sprite.collide_mask(self, res))
+
         self.rect = self.rect.move(0, -1)
         return res
+
+    def step(self, direction):
+        """Шаг персонажа, т.е. смена его картинки"""
+
+        self.image = eval(f"pygame.transform.scale(Enemy.image{self.active_step + 1}, CHARACTER_SIZE)")
+        self.active_step = (self.active_step + 1) % 2
+        self.rotation = RIGHT
+
+        x, y = self.rect.x, self.rect.y
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+        self.rotate(direction)
+
+    def rotate(self, direction):
+        """Поворот персонажа на 180 градусов"""
+
+        if self.rotation != direction:
+            self.rotation = direction
+            self.image = pygame.transform.flip(self.image, True, False)
+
+            x, y = self.rect.x, self.rect.y
+            self.rect = self.image.get_rect()
+            self.rect.x, self.rect.y = x, y
 
 
 screen.fill((255, 255, 255))
@@ -104,8 +181,10 @@ while running:
         if enemy:
             if pygame.key.get_pressed()[pygame.K_LEFT]:
                 enemy.move(x=-10)
+
             elif pygame.key.get_pressed()[pygame.K_RIGHT]:
                 enemy.move(x=10)
+
             elif pygame.key.get_pressed()[pygame.K_SPACE] and enemy.can_jump():
                 enemy.move(y=-60)
             elif enemy.climbing:
